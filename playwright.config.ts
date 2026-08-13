5f2d935c3c657e8b12fe5535d7ed1518cc09d1e5
Praveen Roy <roykumar0593@gmail.com>
2026-08-13 22:25:37 +0530

    added parallel

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,6 +8,19 @@ import { defineConfig, devices } from '@playwright/test';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+// {key : value}
+
+/*
+
+1. fullyParallel = true and workers = 1 - Test case will run in sequential order
+2. fullyParallel = false and workers = 1 - Test case will run in sequential order
+3. fullyParallel = false and workers > 1 - The TEST FILE will be executed in a
+parralel mode but not the test case inside the file
+4. fullyParallel = true and workers > 1 - The TEST CASE insside the file will run 
+in the parallel mode but not the test file
+
+*/
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -20,7 +33,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 5,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -29,7 +42,10 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
+    screenshot: 'on',
+    video : 'on',
+    //headless : false
   },
 
   /* Configure projects for major browsers */
@@ -39,15 +55,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
