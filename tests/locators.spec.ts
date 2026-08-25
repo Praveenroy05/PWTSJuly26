d96eda373e7214d47445be21979e71338c23a71f
Praveen Roy <roykumar0593@gmail.com>
2026-08-25 19:29:21 +0530

    added xpath

--- tests/locators.spec.ts
+++ tests/locators.spec.ts
@@ -94,7 +94,106 @@ nth(index) - Index starts with 0 - identify any element with the index value
 XPATH
 --------------------------------------------------------------------------
 
+// Absolute xpath -  / - /html/body/div/div/section/section/h2
+// Relative xpath -  // - //h2
+
+
+In xpath the index starts from 1
+
+// Syntax:
+
+1. //*[@attribute='value'] - Slower in terms of finding
+
+2. //tagname[@attribute='value'] - faster
+
+(//input)[2]
+
+AXES - xpath
+
+3. Parent to child - by using /(immediate child) or //(Any children - can be descandant)
+
+//div[@class='practice-form-wrapper']/h5 OR
+
+//div[@id='root']//h5
+
+(//div[@class='practice-form-wrapper']/h5)[1]
+
+
+4. Moving from one sibling to another sibling
+
+Syntax:
+
+//tagname[@attribute='value']/following-sibling::tagname[@attribute='value']
+
+//tagname[@attribute='value']/preceding-sibling::tagname[@attribute='value']
+
+//div[@id='userName-wrapper']/following-sibling::div[@id='userEmail-wrapper']
+
+//div[@id='userEmail-wrapper']/preceding-sibling::div[@id='userName-wrapper']
+
+//div[@id='userName-wrapper']/following-sibling::div - It might identify mutliple elements
+
+//div[@id='userName-wrapper']/following-sibling:://h2[text()='Test login']
+
+
+
+5. Identify the element by using text of an element
+
+Syntax:
+
+ 1. By using exact text value of an element
+//* or tagname[text()='Exact text value of an element']
+
+//h2[text()='Test login']
+
+// 2. By using partial text value of an element
+
+//* or tagname[contains(text(), 'Partial text value of an element')]
+
+//*[contains(text(), 'negative LogIn tests')]
+
+
+This is a simple Login page. Students can use this page 
+to practice writing simple positive and negative LogIn tests. 
+Login functionality is something that most of the test automation 
+engineers need to automate.
+
+count(//div) - Total number of element matching with div as a tagname
+
+
 Assignment - Practice 20 different locators by following all the above 4 properties
+
+Assignment 2 - To write the locator to find out the value of CPU% for Firefox
+
+
+
+Example in the below table: 9.0%
+
+Dynamic Web Table:
+
+Name	CPU (%)	Memory (MB)	Disk (MB/s)	Network (Mbps)
+System	6.0%	51.9 MB	0.91 MB/s	5.4 Mbps
+Internet Explorer	7.1%	33.2 MB	0.12 MB/s	4.1 Mbps
+Firefox	9.0%	39.0 MB	0.55 MB/s	9.6 Mbps
+Chrome	9.9%	38.2 MB	0.41 MB/s	0.4 Mbps
+
+id = name453656
+
+//*[contains(@id, 'name')]
+
+
+
+// ancestor-or-self - //*[contains(@id, 'user')]/ancestor::*
+
+// Identify the current element and all the parent elements (ancestor)
+
+
+
+
+
+3. Playwright getBy Locators
+
+    
 
 
 
